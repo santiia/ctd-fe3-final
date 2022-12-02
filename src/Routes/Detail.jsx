387ee4bf43890550b32c4ccaa5d635c23440ea72
SoyJeroMe <ojeronimo360@gmail.com>
2022-12-02 15:06:09 -0500

    corregir error

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { useParams } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
+  const params = useParams();
+  console.log(params);
+
+  // let result = data?.find(item => item.id === params.id)
+
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
   return (
@@ -13,7 +17,7 @@ const Detail = () => {
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
