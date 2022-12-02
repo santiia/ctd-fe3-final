9056cfcc056b8fd58b915701324fbf6025625294
santiia <santiago.ortigoza@cun.edu.co>
2022-12-01 19:15:35 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home"
+import Contact from "./Routes/Contact"
+import Favs from "./Routes/Contact"
+
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path="/home" element={<Home/>}/>
+            <Route path="/contacto" element={<Contact/>}/>
+            <Route path="/favs" element={<Favs/>}/>
+          </Routes>
           <Footer/>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,6 +8,10 @@ const Navbar = () => {
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to ="/home"></Link>
+      <Link to ="/dentist/:id"></Link>
+      <Link to ="/contacto"></Link>
+      <Link to ="/favs"></Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
