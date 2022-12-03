7898ee0096c2f2b47ac750d3c281c00708c945b1
SoyJeroMe <ojeronimo360@gmail.com>
2022-12-03 08:52:03 -0500

    agregar validacion en formulario de contacto

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,48 +1,62 @@
 import React from "react";
 import { useState } from "react";
 
-
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
   const [solicitud, setSolicitud] = useState({
     nombre: "",
-    email: ""
-  })
+    email: "",
+  });
 
-  const [meesage, setMessage] = useState("")
+  const [meesage, setMessage] = useState("");
   // setear valores en el hook
   const HandleChange = (e) => {
     const nombre = e.target.name;
     let value = e.target.value;
-    setSolicitud({...solicitud, [nombre] : value})
-    console.log(solicitud)
-  }
+    setSolicitud({ ...solicitud, [nombre]: value });
+    console.log(solicitud);
+  };
   // validar que el email tenga el formato correcto
   const emailValidation = () => {
     const regEx = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
-    if(regEx.test(solicitud.email)){
-      return true
-    } else{
-      return false
+    if (regEx.test(solicitud.email)) {
+      return true;
+    } else {
+      return false;
     }
-
-  }
+  };
 
   const HandleSubmit = (e) => {
-    e.preventDefault()
-    if(solicitud.nombre.length > 5 && emailValidation() == false){
-        setMessage("Gracias "+ solicitud.nombre  + " te contactaremos cuanto antes via email") 
-    } else{ 
-      setMessage("Por favor verifique su informacion nuevamente")
+    e.preventDefault();
+    if (solicitud.nombre.length > 5 && emailValidation() == true) {
+      setMessage(
+        "Gracias " +
+          solicitud.nombre +
+          " te contactaremos cuanto antes via email"
+      );
+    } else {
+      setMessage("Por favor verifique su informacion nuevamente");
     }
-  }
+  };
 
   return (
     <div>
       <form>
-        <input type="text" placeholder="Nombre" name="nombre" onChange={(e) => HandleChange(e)}></input>
-        <input type="text" placeholder="Email" name="email" onChange={(e) => HandleChange(e)}></input>
-        <button type="submit" onClick={e => HandleSubmit(e)}>Envia</button>
+        <input
+          type="text"
+          placeholder="Nombre"
+          name="nombre"
+          onChange={(e) => HandleChange(e)}
+        ></input>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={(e) => HandleChange(e)}
+        ></input>
+        <button type="submit" onClick={(e) => HandleSubmit(e)}>
+          Envia
+        </button>
       </form>
       <h3>{meesage}</h3>
     </div>
