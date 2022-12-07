0d3ed5f626a37d3799fe34d40030749fce0f78e6
SoyJeroMe <ojeronimo360@gmail.com>
2022-12-06 19:53:28 -0500

    agregar variaciones para realizar los estilos de contact

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Form from '../Components/Form'
+import {contactSyles} from '../styles/contact.css'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <h2>Want to know more?</h2>
       <p>Send us your questions and we will contact you</p>
-      <Form/>
+      <div className="contact-form">
+        <Form />
+      </div>
     </div>
   )
 }
