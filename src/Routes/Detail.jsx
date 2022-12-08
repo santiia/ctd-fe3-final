45e040048f6d944fbbbbaf2cac1f02083c6c14b2
SoyJeroMe <ojeronimo360@gmail.com>
2022-12-08 15:27:15 -0500

    agregar estilos de details

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,36 +1,37 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios"
-import { useState } from "react";
+import React from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
+import {useState} from 'react'
+import {detailStyles} from '../styles/detail.css'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
-  const {dentistId}= useParams();
-  console.log(dentistId);
-  const url = "https://jsonplaceholder.typicode.com/users/" + dentistId;
+  const {dentistId} = useParams()
+  console.log(dentistId)
+  const url = 'https://jsonplaceholder.typicode.com/users/' + dentistId
   const [data, setData] = useState([])
 
   React.useEffect(() => {
     axios.get(url).then((response) => {
-      setData(response.data);
-    });
-  }, [url]);
+      setData(response.data)
+    })
+  }, [url])
   // let result = data?.find(item => item.id === params.id)
 
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
   return (
-    <>
-      <h1>Detail Dentist id </h1>
+    <div className="dentist-detail">
+      <h1 className="title-detail">Detail Dentist id </h1>
       <h2>{data.name}</h2>
       <h2>{data.email}</h2>
       <h2>{data.phone}</h2>
       <h2>{data.website}</h2>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default Detail;
+export default Detail
